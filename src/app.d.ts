6fb0aa5309001737242e15f622bbd67548a5c0a8
codie <bishwasbh@outlook.com>
2023-08-24 08:43:20 +0545

    UI Updated

---
+++ src/app.d.ts
@@ -0,0 +1,12 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information projects these interfaces
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface Platform {}
+	}
+}
+
+export {};
---
+++ svelte.config.js
@@ -0,0 +1,26 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: [
+		vitePreprocess({}),
+		preprocess({
+			postcss: true
+		})
+	],
+
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$item: './src/item',
+			$fonts: './src/fonts',
+			$utils: './src/utils',
+			$styles: './src/styles'
+		}
+	},
+	plugins: []
+};
+
+export default config;
