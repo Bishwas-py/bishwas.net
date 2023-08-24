7b2e1b0995543de6f652d90b3001526fb0480b16
codie <bishwasbh@outlook.com>
2023-08-24 08:43:20 +0545

    UI Updated

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Hero from "$item/main/Hero.svelte";
+    import {browser} from "$app/environment";
+    import Content from "$item/main/Content.svelte";
+</script>
+
+<svelte:head>
+    <title>Bishwas Bhandari | Intro</title>
+</svelte:head>
+
+
+{#key browser}
+    <Hero/>
+    <hr class="w-full mt-11 border-blue-600/5"/>
+    <Content/>
+{/key}
---
+++ tailwind.config.cjs
@@ -0,0 +1,40 @@
+
+const config = {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                'play-fair': ['play-fair', 'sans-serif'],
+                combo: ['combo', 'sans-serif'],
+                lato: ['lato', 'sans-serif'],
+                ssp: ['source-sans-pro', 'sans-serif'],
+                inter: ['inter', 'sans-serif'],
+                poppins: ['poppins', 'sans-serif'],
+                ds: ['dancing-script', 'sans-serif'],
+                'noto-sans': ['noto-sans', 'sans-serif'],
+            },
+
+            dropShadow: {
+                'white-sm': '0 1px 2px rgba(255,255,255, 0.05)',
+                'white-md': '0 4px 6px rgba(255,255,255, 0.1)',
+                'white-lg': '0px 15px 15px rgba(255,255,255,1)',
+                'white-xl': '0px 20px 25px rgba(255,255,255,1)',
+                'dark-sm': '0 1px 2px rgba(0,0,0, 0.05)',
+                'dark-md': '0 4px 6px rgba(0,0,0, 0.1)',
+                'dark-lg': '0px 15px 15px rgba(0,0,0,1)',
+                'dark-xl': '0px 20px 25px rgba(0,0,0,1)',
+            },
+            colors: {
+                'primary': '#202124',
+                'secondary': '#3c4043',
+                'accent': '#191919',
+            }
+        }
+    },
+
+    plugins: [],
+    darkMode: 'class'
+};
+
+module.exports = config;
