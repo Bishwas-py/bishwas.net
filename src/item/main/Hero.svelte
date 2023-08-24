b48e2e872aec5a9ce8d6340848327c9cd931a3a1
codie <bishwasbh@outlook.com>
2023-08-24 21:47:35 +0545

    Wo/Man is now only enabled when hovering over is done, as I have to put some extra efforts to get to ladies.

--- src/item/main/Hero.svelte
+++ src/item/main/Hero.svelte
@@ -10,7 +10,10 @@
             <img src="/me-bishwas.png" alt="Bishwas Bhandari" class="hero-image"/>
         </div>
         <div class="hero-text">
-            <span class="text-5xl duration-200 sm:text-6xl md:text-7xl font-noto-sans px-0 mb-1" in:fade={{delay: 100}}>Hey, Human!</span>
+            <span class="text-5xl duration-200 sm:text-6xl md:text-7xl font-noto-sans px-0 mb-1" in:fade={{delay: 100}}>
+                Hey,
+                <span class="group"><span class="group-hover:inline hidden">Wo/</span><span class="peer">Man</span></span>!
+            </span>
             <h1 class="text-2xl font-noto-sans mb-4 ml-2" in:fade={{delay: 200}}>I am Bishwas.</h1>
             <p class="text-lg font-noto-sans ml-1" in:fade={{delay: 450}}>
                 A Full-Stack Web developer doing Django Python, Svelte/Kit, Ruby on Rails, React.js &
