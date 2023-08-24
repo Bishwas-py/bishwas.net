55b4f38c76bcd88a0d0d27da5817bb5e997ca274
codie <bishwasbh@outlook.com>
2023-08-24 09:00:33 +0545

    scaling transition added

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,1 +1,22 @@
-About Page
+<script lang="ts">
+
+</script>
+
+<div class="projects-tab">
+    <h1 class="text-2xl font-bold">Projects</h1>
+    <p class="text-gray-500 dark:text-gray-400">Here are some of my projects</p>
+</div>
+
+<style lang="postcss">
+    .projects-tab {
+        @apply mt-2 mb-2;
+        @apply bg-white dark:bg-gray-800;
+        @apply rounded-lg;
+        @apply shadow-md;
+        @apply p-4;
+        @apply border border-gray-200 dark:border-gray-700;
+        @apply dark:text-gray-200;
+        @apply dark:hover:text-gray-100;
+        @apply w-full max-w-3xl;
+    }
+</style>
