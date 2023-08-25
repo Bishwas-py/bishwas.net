a45bd2476e08be3e864ea5a66c33d8c78c325416
codie <bishwasbh@outlook.com>
2023-08-25 15:19:37 +0545

    Me-logo update to be fixed.

--- src/item/Navbar.svelte
+++ src/item/Navbar.svelte
@@ -6,7 +6,7 @@
 <nav>
 
     {#if $page.url.pathname !== "/"}
-        <div class="md:absolute md:top-0 md:left-7 mr-7 md:mr-0 hero-image-container w-7 h-7"
+        <div class="fixed top-4 md:absolute md:top-0 md:left-7 mr-7 md:mr-0 hero-image-container w-7 h-7"
              transition:fly={{x: -100, duration: 500}}>
             <img src="/me-bishwas.jpg" alt="Bishwas Bhandari" class="hero-image"/>
         </div>
