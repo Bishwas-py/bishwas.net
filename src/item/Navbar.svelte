626f24c5ea78b7a5b9a8c7262cd41ac8f322588e
codie <bishwasbh@outlook.com>
2023-08-25 14:06:18 +0545

    Socials added and experience added.

--- src/item/Navbar.svelte
+++ src/item/Navbar.svelte
@@ -6,7 +6,7 @@
 <nav>
 
     {#if $page.url.pathname !== "/"}
-        <div class="absolute top-0 left-7 hero-image-container drop-shadow-none w-7 h-7"
+        <div class="md:absolute md:top-0 md:left-7 mr-7 md:mr-0 hero-image-container w-7 h-7"
              transition:fly={{x: -100, duration: 500}}>
             <img src="/me-bishwas.png" alt="Bishwas Bhandari" class="hero-image"/>
         </div>
@@ -15,29 +15,27 @@
     <ul>
         <li class:active={$page.url.pathname === "/"}><a href="/">Home</a></li>
         <li class:active={$page.url.pathname === "/projects"}><a href="/projects">Projects</a></li>
+        <li class:active={$page.url.pathname === "/experience"}><a href="/experience">Experience</a></li>
     </ul>
 </nav>
 
 <style lang="postcss">
 
     nav {
-        @apply flex flex-col relative items-center;
+        @apply flex justify-center gap-4 flex-col relative items-center;
         @apply mt-16 md:mt-14 py-1;
     }
 
     ul {
-        @apply flex;
-        @apply gap-7;
+        @apply flex flex-wrap;
+        @apply md:gap-7 gap-4;
     }
 
     ul li {
         @apply dark:hover:drop-shadow-white-lg hover:drop-shadow-dark-lg;
         @apply dark:hover:text-blue-200;
         @apply duration-300;
-    }
-
-    ul li.hero-image-container {
-        @apply bg-green-500;
+        @apply md:text-base text-sm;
     }
 
     ul li.active {
@@ -45,7 +43,8 @@
     }
 
     ul li a {
-        @apply py-2 px-3;
+        @apply md:py-2 md:px-3;
+        @apply py-0.5 px-1;
     }
 
 </style>
