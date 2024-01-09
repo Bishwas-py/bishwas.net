05ae4239b9f6c183b78c0a6a292b7d6ad10d1d78
Bishwas Bhandari <bishwasbh@gmail.com>
2024-01-09 19:47:41 +0545

    Remove redundant font styles and assets
    The commit removes numerous font styles and associated assets that are no longer in use. This includes font styles and corresponding assets for combo, dancing-script, inter, lato, play-fair, poppins, and source-sans-pro. This cleanup helps improve the project's maintainability by eliminating resources that are unnecessary or obsolete.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,11 @@
-
 const config = {
     content: ['./src/**/*.{html,js,svelte,ts}'],
 
     theme: {
         extend: {
             fontFamily: {
-                'play-fair': ['play-fair', 'sans-serif'],
-                combo: ['combo', 'sans-serif'],
-                lato: ['lato', 'sans-serif'],
-                ssp: ['source-sans-pro', 'sans-serif'],
-                inter: ['inter', 'sans-serif'],
-                poppins: ['poppins', 'sans-serif'],
-                ds: ['dancing-script', 'sans-serif'],
                 'noto-sans': ['noto-sans', 'sans-serif'],
+                'work-sans': ['work-sans', 'sans-serif'],
             },
 
             dropShadow: {
