3cd4e68953c4075707a44020318c2f3f12253dde
Bishwas Bhandari <bishwasbh@gmail.com>
2025-02-14 15:52:55 +0545

    feat: links updated

--- src/routes/svelte-developer/cta.svelte
+++ src/routes/svelte-developer/cta.svelte
@@ -12,6 +12,8 @@
 	<div class="flex flex-wrap gap-4 items-center justify-between w-full sm:w-auto">
 		<a
 			href="https://calendar.app.google/GN4FVeLyi4YpHpRu9"
+			rel="noopener noreferrer"
+			target="_blank"
 			class="group w-min flex items-center justify-center gap-3 px-6 py-2 bg-gradient-to-r from-orange-500 to-rose-500 hover:from-orange-600 hover:to-rose-600 text-white rounded-xl transition-all duration-300 shadow-lg shadow-orange-500/20 hover:shadow-orange-500/30"
 		>
 			<FluentMeetNow32Filled display class="w-5 h-5"/>
@@ -20,8 +22,6 @@
 
 		<a
 			href="mailto:yo@bishwas.net"
-			target="_blank"
-			rel="noopener noreferrer"
 			class="group w-min flex items-center justify-center gap-3 px-6 py-2 bg-white dark:bg-gray-800/50 text-gray-900 dark:text-white rounded-xl transition-all duration-300 "
 		>
 			<FluentMail32Filled display class="w-5 h-5"/>
